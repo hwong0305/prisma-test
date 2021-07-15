3c8d1a14542b8c179766dfb67cb9d138709986dd
hwong0305 <hwong.0305@gmail.com>
2021-07-14 22:54:59 -0700

    Added middleware on express

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
   res.send({message: 'Welcome to the beginning of nuthingless'});
 });
