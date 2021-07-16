515342c9ba89fc0faeb65ce2f1992862e1515a09
hwong0305 <hwong.0305@gmail.com>
2021-07-15 22:02:36 -0700

    Added morgan for development logging

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
