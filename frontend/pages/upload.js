ff01b78a798d092ddde19986c7a902888652b197
hwong0305 <hwong.0305@gmail.com>
2021-07-24 17:30:26 -0700

    Updated spacing

--- frontend/pages/upload.js
+++ frontend/pages/upload.js
@@ -30,6 +30,20 @@ const useStyles = makeStyles(theme => ({
   input: {
     textOverflow: 'ellipsis',
   },
+  label: {
+    display: 'block',
+    position: 'absolute',
+    fontFamily: 'Roboto',
+    fontSize: '16px',
+    transformOrigin: 'top left',
+    background: 'white',
+    boxSizing: 'border-box',
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    transform: 'translate(0px, -25px) scale(0.75)',
+    zIndex: 1,
+    color: 'inherit',
+  },
   paper: {
     padding: '3rem',
     margin: theme.spacing(8, 4),
@@ -37,8 +51,21 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
+  submit: {
+    marginTop: theme.spacing(3),
+  },
   upload: {
-    padding: theme.spacing(1),
+    border: 'solid 1px rgba(0,0,0,0.33)',
+    color: 'rgba(0,0,0,0.54)',
+    borderRadius: '0.5rem',
+    padding: theme.spacing(2),
+    position: 'relative',
+    boxSizing: 'border-box',
+    '&:hover': {
+      borderColor: theme.palette.primary.dark,
+      color: theme.palette.primary.dark,
+      // borderWidth: '2px',
+    },
   },
 }));
 
@@ -67,16 +94,36 @@ export default function Upload() {
                 Sell your Product
               </Typography>
               <form className={classes.form} noValidate>
-                <TextField variant="outlined" label="Name" fullWidth />
-                <TextField variant="outlined" label="Price" fullWidth />
-                <TextField variant="outlined" label="Description" multiline fullWidth />
-                <Grid container item xs={12} className={classes.upload} spacing={2}>
+                <TextField
+                  variant="outlined"
+                  label="Name"
+                  InputLabelProps={{shrink: true}}
+                  fullWidth
+                />
+                <TextField
+                  variant="outlined"
+                  label="Price"
+                  InputLabelProps={{shrink: true}}
+                  fullWidth
+                />
+                <TextField
+                  variant="outlined"
+                  label="Description"
+                  InputLabelProps={{shrink: true}}
+                  multiline
+                  fullWidth
+                />
+                <Grid container item xs={12} className={classes.upload} spacing={0}>
                   <Grid item xs={4}>
+                    <label htmlFor="file" className={classes.label}>
+                      Upload
+                    </label>
                     <input
                       accept="image/*"
                       id="contained-button-file"
                       multiple
                       className={classes.htmlInput}
+                      name="file"
                       type="file"
                       onChange={onChange}
                     />
@@ -96,7 +143,14 @@ export default function Upload() {
                     />
                   </Grid>
                 </Grid>
-                <Grid item container justifyContent="flex-end" alignItems="center" spacing={2}>
+                <Grid
+                  item
+                  container
+                  justifyContent="flex-end"
+                  alignItems="center"
+                  spacing={2}
+                  className={classes.submit}
+                >
                   <Button variant="contained" color="primary" component="span">
                     Submit
                   </Button>
